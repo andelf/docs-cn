fa318b07b7e4a5e83a4bb0991a5cab39a1d14611
andelf <andelf@gmail.com>
2016-12-31 14:06:00 +0800

    ci: fix font error

--- scripts/generate_pdf.sh
+++ scripts/generate_pdf.sh
@@ -3,12 +3,14 @@
 set -e
 # test passed in pandoc 1.19.1
 
+_version_tag="$(date '+%Y%m%d').$(git rev-parse --short HEAD)"
+
 pandoc -N --toc --smart --latex-engine=xelatex \
     --template=templates/template.tex \
     --listings \
     -V title="TiDB 中文手册" \
     -V author="PingCAP Inc." \
-    -V date="v1.0-rc1+20161231" \
+    -V date="v1.0~rc1+${_version_tag}" \
     -V CJKmainfont='Microsoft YaHei' \
     -V mainfont='Tsentsiu Sans HG' \
     -V sansfont='Tsentsiu Sans HG' \
