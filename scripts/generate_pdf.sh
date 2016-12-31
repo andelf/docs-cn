1b92710572b35700d1fde46e3a588725a01ffe08
andelf <andelf@gmail.com>
2016-12-31 14:08:51 +0800

    ci: use wqy font

--- scripts/generate_pdf.sh
+++ scripts/generate_pdf.sh
@@ -3,6 +3,9 @@
 set -e
 # test passed in pandoc 1.19.1
 
+MAINFONT="WenQuanYi Micro Hei"
+MONOFONT="WenQuanYi Micro Hei Mono"
+
 _version_tag="$(date '+%Y%m%d').$(git rev-parse --short HEAD)"
 
 pandoc -N --toc --smart --latex-engine=xelatex \
@@ -11,9 +14,9 @@ pandoc -N --toc --smart --latex-engine=xelatex \
     -V title="TiDB 中文手册" \
     -V author="PingCAP Inc." \
     -V date="v1.0~rc1+${_version_tag}" \
-    -V CJKmainfont='Microsoft YaHei' \
-    -V mainfont='Tsentsiu Sans HG' \
-    -V sansfont='Tsentsiu Sans HG' \
-    -V monofont='Tsentsiu Sans Console HG' \
+    -V CJKmainfont="${MAINFONT}" \
+    -V mainfont="${MAINFONT}" \
+    -V sansfont="${MAINFONT}" \
+    -V monofont="${MONOFONT}" \
     -V geometry:margin=1in \
     doc.md -o output.pdf
